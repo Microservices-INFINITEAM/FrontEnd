aa9477e5cfa44c67e872faa3adb0a0ee1a23639f
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2022-12-02 19:51:58 +0900

    first commit

---
+++ msa-mil/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => {
+    return (
+        <div className="main">
+       gd
+        </div>
+    );
+};
+
+export default App;
